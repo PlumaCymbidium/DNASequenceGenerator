bdc571c420c9fecbdb5af0a2e0584031f51d1981
Pluma <chenzezhong@cxist.com>
2020-01-13 20:15:45 +0800

    initial project

---
+++ Pluma.DNASerialGenerator/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Pluma.DNASerialGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtx_OriginSerials = new System.Windows.Forms.RichTextBox();
+            this.lbl_Input = new System.Windows.Forms.Label();
+            this.lbl_Output = new System.Windows.Forms.Label();
+            this.rtx_OutputSerials = new System.Windows.Forms.RichTextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtx_OriginSerials
+            // 
+            this.rtx_OriginSerials.Location = new System.Drawing.Point(77, 81);
+            this.rtx_OriginSerials.Name = "rtx_OriginSerials";
+            this.rtx_OriginSerials.Size = new System.Drawing.Size(729, 167);
+            this.rtx_OriginSerials.TabIndex = 0;
+            this.rtx_OriginSerials.Text = "";
+            // 
+            // lbl_Input
+            // 
+            this.lbl_Input.AutoSize = true;
+            this.lbl_Input.Location = new System.Drawing.Point(77, 47);
+            this.lbl_Input.Name = "lbl_Input";
+            this.lbl_Input.Size = new System.Drawing.Size(90, 17);
+            this.lbl_Input.TabIndex = 1;
+            this.lbl_Input.Text = "Input serial(s):";
+            // 
+            // lbl_Output
+            // 
+            this.lbl_Output.AutoSize = true;
+            this.lbl_Output.Location = new System.Drawing.Point(77, 269);
+            this.lbl_Output.Name = "lbl_Output";
+            this.lbl_Output.Size = new System.Drawing.Size(100, 17);
+            this.lbl_Output.TabIndex = 2;
+            this.lbl_Output.Text = "Output serial(s):";
+            // 
+            // rtx_OutputSerials
+            // 
+            this.rtx_OutputSerials.Location = new System.Drawing.Point(77, 313);
+            this.rtx_OutputSerials.Name = "rtx_OutputSerials";
+            this.rtx_OutputSerials.Size = new System.Drawing.Size(729, 193);
+            this.rtx_OutputSerials.TabIndex = 3;
+            this.rtx_OutputSerials.Text = "";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(731, 527);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 26);
+            this.btn_submit.TabIndex = 4;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(601, 527);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 26);
+            this.btn_Clear.TabIndex = 5;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(962, 597);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.rtx_OutputSerials);
+            this.Controls.Add(this.lbl_Output);
+            this.Controls.Add(this.lbl_Input);
+            this.Controls.Add(this.rtx_OriginSerials);
+            this.Name = "MainForm";
+            this.Text = "DNA Serial Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtx_OriginSerials;
+        private System.Windows.Forms.Label lbl_Input;
+        private System.Windows.Forms.Label lbl_Output;
+        private System.Windows.Forms.RichTextBox rtx_OutputSerials;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 生成反向互补序列
+# DNA Serial Genrator
 ## Requirements analysis
 
 ### Original requirement
@@ -8,25 +8,27 @@
 
 ### BA
 
-no big deal, just for fun :joy: :joy: :joy:
+of no damn use, just for fun :joy: :joy: :joy:
 
 ### References
 
-> <http://wikihow.com/Find-the-Reverse-Complement-of-a-DNA-Sequence>
-> [FASTA format :link:](https://en.wikipedia.org/wiki/FASTA_format)
+> <http://wikihow.com/Find-the-Reverse-Complement-of-a-DNA-Sequence>  
+> [FASTA format :link:](https://en.wikipedia.org/wiki/FASTA_format)  
 
 ## Solutions
 
 ### Solution A
 
 **use online script** (click the following links)
-> [BioInformatics :link: ](http://www.bioinformatics.org/sms/rev_comp.html)
-> [GenScript :link: ](https://www.genscript.com/sms2/rev_comp.html)
+> [BioInformatics :link: ](http://www.bioinformatics.org/sms/rev_comp.html)  
+> [GenScript :link: ](https://www.genscript.com/sms2/rev_comp.html)  
 
 * 可以支持长度为800bp+的FASTA格式的序列，但不支持自动批量导入导出。
 * 手动拼接可以实现批量导入导出。
 具体方法是将多个DNA序列序列化为FASTA格式的序列直接拼接。
-> FASTA format is a text-based format for representing either nucleotide sequences or peptide sequences, in which base pairs or amino acids are represented using single-letter codes. A sequence in FASTA format begins with a single-line description, followed by lines of sequence data. The description line is distinguished from the sequence data by a greater-than (">") symbol in the first column. It is recommended that all lines of text be shorter than 80 characters in length.
+> FASTA format is a **text-based** format for representing either nucleotide sequences or peptide sequences, in which base pairs or amino acids are represented using single-letter codes. 
+> A sequence in FASTA format begins with **a single-line description**, followed by lines of sequence data. 
+> The description line is distinguished from the sequence data by a **greater-than (">") symbol** in the first column. It is recommended that all lines of text be shorter than 80 characters in length.(800bp要不要分割一下呢 :sweat_smile: )
 
 > An example sequence in FASTA format is:
 ```
@@ -41,11 +43,16 @@ LRYLRYRAEKDPARGMFKRFYGAEWTEEYIHGFLFDLERKLTVVK
 > * a single hyphen or dash can be used to represent a gap of indeterminate length;
 > * in amino acid sequences, U and * are acceptable letters (see below).
 > * any numerical digits in the query sequence should either be removed or replaced by appropriate letter codes (e.g., N for unknown nucleic acid residue or X for unknown amino acid residue).
-> [:link:](https://zhanglab.ccmb.med.umich.edu/FASTA/)
+> [:link:](https://zhanglab.ccmb.med.umich.edu/FASTA/)  
 
 
 ### Solution B
 
+#### Development log
+[Updates to .NET Core Windows Forms designer in Visual Studio 16.5 Preview 1](https://devblogs.microsoft.com/dotnet/updates-to-net-core-windows-forms-designer-in-visual-studio-16-5-preview-1/)
+Update Visual Studio Enterprise 2019 16.4.2 -> Preview 16.5.0 Preview 1.0
+
 
 ### Solution C
 
+use python command
